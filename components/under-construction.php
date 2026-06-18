c4bd922e18a0c06e0fb3bab51ac3ff35c3e08b65
Glenn-IT <glenard2308@gmail.com>
2026-06-18 23:15:32 +0800

    feat: implement v1.09 - unlock New Application

--- components/under-construction.php
+++ components/under-construction.php
@@ -1,5 +1,5 @@
 <?php
-define('CURRENT_VERSION', 'v1.08');
+define('CURRENT_VERSION', 'v1.09');
 ?>
 <!doctype html>
 <html lang="en">
--- views/user/new-application.php
+++ views/user/new-application.php
@@ -1,4 +1,3 @@
-<?php require_once '../../components/under-construction.php'; ?>
 <?php
 $pageTitle      = 'New Application — Crop Insurance';
 $basePath       = '../../';
